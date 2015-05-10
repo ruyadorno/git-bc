ef4d9efafdf28c314eaea586e527adc9f300a6fa
Ruy Adorno <ruyadorno@hotmail.com>
2015-05-10 12:46:45 -0400

    Initial implementation

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
-#!/usr/bin/env node
-
 'use strict';
 
-module.exports = function () {
+var events = require('events');
+
+var inquirer = require('inquirer');
+
+
+// ---
+
+
+module.exports = function gitBranchCheckout(exec, options) {
+
+  var result = new events.EventEmitter();
+
+  function validateExec(err, stdout, stderr) {
+    if (err) {
+      result.emit('error', stderr);
+      return true;
+    }
+  }
+
+  function onGitBranch(err, stdout, stderr) {
+
+    if (validateExec(err, stdout, stderr)) {
+      return;
+    }
+
+    if (stdout) {
+      promptList(
+        stdout.split('\n').filter(function (line) {
+          return line;
+        })
+      );
+    } else {
+      result.emit('warn', 'No git branch was found on the current folder');
+    }
+  }
+
+  function promptList(branches) {
+    result.prompt = inquirer.prompt([{
+      type: 'list',
+      name: 'branches',
+      message: 'Select a branch to checkout:',
+      choices: branches.map(function (branchName) {
+        return {
+          name: branchName.trim(),
+          value: branchName.trim()
+        };
+      })
+    }], onBranchChosen);
+  }
+
+  function onBranchChosen(answers) {
+    exec('git checkout ' + answers.branches, onCheckout.bind(null, answers.branches));
+  }
+
+  function onCheckout(branchName, err, stdout, stderr) {
+    validateExec(err, stdout, stderr);
+    result.emit('success', 'Switched to branch \'' + branchName.trim() + '\'');
+  }
+
+  exec('git branch', onGitBranch);
+
+  return result;
 };
 
--- test.js
+++ test.js
@@ -1,9 +1,83 @@
 'use strict';
 
 var assert = require('assert');
+
 var gitBc = require('./');
 
-it('should do something', function () {
-	assert.strictEqual(true, false);
+
+// ---
+
+
+function mockExec(mockResults) {
+
+  return function exec(ref, cb) {
+
+    var result = mockResults[ref];
+
+    setTimeout(function () {
+      cb(result.error, result.stdout, result.stderr);
+    }, 1);
+  };
+
+}
+
+
+// ---
+
+
+it('should checkout selected branch', function (done) {
+
+  var mockResult = {
+    'git branch': {
+      stdout:
+        ' * master\n' +
+        ' develop\n' +
+        ' feature-1\n'
+    },
+    'git checkout develop': {
+      stdout: ''
+    }
+  };
+  var result = gitBc(mockExec(mockResult));
+
+  // Validates success message
+  result.on('success', function (msg) {
+    assert.strictEqual(msg, 'Switched to branch \'develop\'');
+    done();
+  });
+
+  // Simulates user interaction
+  setTimeout(function () {
+    result.prompt.rl.emit("keypress", "", { name : "down" });
+    result.prompt.rl.emit("line");
+  }, 10);
+
+});
+
+
+// ---
+
+
+it('should notificate errors', function (done) {
+
+  var errorMsg =
+    'fatal: Not a git repository (or any of the parent directories): .git';
+  var mockResult = {
+    'git branch': {
+      error: true,
+      stderr: errorMsg
+    },
+    'git checkout develop': {
+      stdout: ''
+    }
+  };
+  var result = gitBc(mockExec(mockResult));
+
+  // Validates error message
+  result.on('error', function (msg) {
+    assert.strictEqual(msg, errorMsg);
+    done();
+  });
+
 });
 
